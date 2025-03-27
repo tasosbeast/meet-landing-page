ce7be7dd71b68777c435c9e1319bb9363178c87f
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-03-27 23:22:20 +0200

    feat: enhance StoryContent component with customer testimonial and button

--- src/components/StoryContent.jsx
+++ src/components/StoryContent.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./_Story.scss";
 
 const StoryContent = () => {
-  return <div className="story__content">Customers</div>;
+  return (
+    <div className="story__content">
+      <h3 className="heading-3 u-margin-bottom-small">Happy Customers</h3>
+      <h2 className="heading-2 heading-2--dark u-margin-bottom-medium ">
+        &ldquo;The best decision of our lives&rdquo;
+      </h2>
+
+      <p className="story__text">
+        We found our dream home within just two weeks of working with Nexter.
+        The process was seamless, and our realtor's expertise helped us secure
+        the perfect property in our ideal neighborhood at a great price.
+      </p>
+      <button className="btn">Find your own home</button>
+    </div>
+  );
 };
 
 export default StoryContent;
