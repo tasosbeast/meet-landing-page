af82716e72e657711fb608b6ae72de8785ae5877
ΑΝΑΣΤΑΣΙΟΣ ΝΤΑΝΤΟΣ <antantos@iekdelta.gr>
2025-03-28 18:42:39 +0200

    feat: add images to StoryPictures component for enhanced visuals

--- src/components/StoryPictures.jsx
+++ src/components/StoryPictures.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./_Story.scss";
+import story1 from "../assets/img/story-1.jpeg";
+import story2 from "../assets/img/story-2.jpeg";
 
 const StoryPictures = () => {
-  return <div className="story__pictures">story__pictures</div>;
+  return (
+    <div className="story__pictures">
+      <img
+        src={story1}
+        alt="Couple with new house"
+        className="story__img--1 story__img"
+      />
+      <img src={story2} alt="New house" className="story__img--2 story__img" />
+    </div>
+  );
 };
 
 export default StoryPictures;
